798aa5b949c3725fd99f057e4db9aa8c00ebf665
Max Kadel <max@curationexperts.com>
2019-12-14 22:05:31 -0500

    circleci and rspec config

---
+++ spec/system/front_page_spec.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+require 'rails_helper'
+
+RSpec.describe 'front page', type: :system do
+  it 'has expected text' do
+    visit "/"
+    expect(page).to have_content 'Welcome!'
+  end
+end
