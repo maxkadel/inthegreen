dcca614ee9fa1c3e5eb7261aff3320edf4012ba8
Max Kadel <max@curationexperts.com>
2019-12-15 17:14:52 -0500

    Expense - "new" form, with model

---
+++ db/migrate/20191215215625_create_expenses.rb
@@ -0,0 +1,13 @@
+class CreateExpenses < ActiveRecord::Migration[6.0]
+  def change
+    create_table :expenses do |t|
+      t.string :expense_name
+      t.money :amount_due_fixed
+      t.money :amount_due_range_low
+      t.money :amount_due_range_high
+      t.date :next_due_date
+
+      t.timestamps
+    end
+  end
+end
