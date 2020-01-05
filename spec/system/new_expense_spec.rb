7361879cce7e3cb3e6fd922a50fb23902978d8a1
Max Kadel <max@curationexperts.com>
2020-01-05 16:01:09 -0500

    Display date and currency values in user-friendly way (#19)

--- spec/system/new_expense_spec.rb
+++ spec/system/new_expense_spec.rb
@@ -3,7 +3,7 @@
 require 'rails_helper'
 require 'spec_helper'
 
-RSpec.describe 'new expense entry', type: :system do
+RSpec.describe 'new expense entry', type: :system, js:true do
 
   it 'has expected text' do
     visit '/expenses/new'
@@ -16,13 +16,15 @@ RSpec.describe 'new expense entry', type: :system do
     fill_in 'expense_amount_due_fixed', with: 22
     fill_in 'expense_amount_due_range_low', with: 15
     fill_in 'expense_amount_due_range_high', with: 25
+    fill_in 'expense_next_due_date', with: '12/15/2019'
     fill_in 'expense_recurrence', with: 'Weekly'
 
     click_on 'Submit'
     expect(page).to have_content 'Water bill'
-    expect(page).to have_content 22
-    expect(page).to have_content 15
-    expect(page).to have_content 25
+    expect(page).to have_content '$22.00'
+    expect(page).to have_content '$15.00'
+    expect(page).to have_content '$25.00'
+    expect(page).to have_content 'December 15, 2019'
     expect(page).to have_content 'Weekly'
   end
 end
